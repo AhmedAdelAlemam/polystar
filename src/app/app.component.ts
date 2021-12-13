06fa8c19be6a404a976d09b6271b82c704369b0c
Ahmed Adel Alemam <ahmedemam755@gmail.com>
2021-12-13 15:27:05 +0200

    Add button to get the data from the api and handling error.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { tram } from './interfaces/interface';
+import { tram } from './interfaces/tramInterface';
 import { AppService } from './services/app.service';
 
 @Component({
@@ -9,15 +9,26 @@ import { AppService } from './services/app.service';
 })
 export class AppComponent {
   constructor(private app_service: AppService) {}
-  title = 'polystart-coding-test';
-  trams: tram[] = [];
-  ngOnInit() {
+  public title = 'polystart-coding-test';
+  public trams: tram[] = [];
+  public tramsFound: boolean = false;
+  public noError = true;
+  public errorMessage = '';
+  ngOnInit() {}
+
+  getTramsData() {
     this.app_service.getApiData().subscribe(
       (data: any) => {
         this.trams = data.ResponseData.Trams;
-        console.log(this.trams);
+        this.tramsFound = true;
       },
-      (error) => console.log(error)
+      (error) => {
+        this.tramsFound = false;
+        this.trams = [];
+        this.noError = false;
+        this.errorMessage = `OOPS Something went Error: ${error.statusText}`;
+        console.log(error);
+      }
     );
   }
 }
