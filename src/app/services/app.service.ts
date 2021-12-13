785dd5a2eff89fcd8dccb375e228cde976574538
Ahmed Adel Alemam <ahmedemam755@gmail.com>
2021-12-13 14:45:06 +0200

    update file names and paths.

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { baseUrl } from './api/api';
+import { baseUrl } from '../models/api/api';
 
 @Injectable({
   providedIn: 'root',
